2ba0d51565dc0e43024ad555aea319e09b45c127
nabeel030 <nabeelahmed030@gmail.com>
2023-05-16 22:00:54 +0500

    Challenge 1: Create migration to support the givne structure

---
+++ database/migrations/2023_05_16_164728_create_schedules_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('employee_id');
+            $table->unsignedBigInteger('location_id');
+            $table->unsignedBigInteger('shift_id');
+ 
+            $table->foreign('employee_id')->references('id')->on('employees');
+            $table->foreign('location_id')->references('id')->on('locations');
+            $table->foreign('shift_id')->references('id')->on('shifts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('schedules');
+    }
+};
