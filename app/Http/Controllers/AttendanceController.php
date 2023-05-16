2a6a674e039749f50c6ed0d7932dbf41f61d6ea6
nabeel030 <nabeelahmed030@gmail.com>
2023-05-16 23:56:00 +0500

    Challenge 2 and 4

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -12,4 +12,31 @@ class AttendanceController extends Controller
         $attendance = Attendance::with('employee')->get();
         return response()->json($attendance);
     }
+
+
+    //Challenge 2 and 4
+    function findDuplicateElements($arr) {
+        $duplicateElements = "";
+    
+        for($i=0; $i<sizeof($arr); $i++) {
+            for($j=$i+1; $j<sizeof($arr); $j++) {
+                if($arr[$i] == $arr[$j]) {
+                  $duplicateElements .=  $arr[$i] . " ";
+                }
+            }
+        }
+        return $duplicateElements;
+    }
+    
+    function groupByOwnersService($arr) {
+        $group = [];
+    
+        foreach($arr as $file => $owner) {
+            if(isset($group[$owner])) {
+                $group[$owner][] = $file;
+            } else {
+                $group[$owner] = [$file];
+            }
+        }
+    }
 }
