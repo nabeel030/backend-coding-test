d632dd23cd41c39774d85752d2c72d715a907895
nabeel030 <nabeelahmed030@gmail.com>
2023-05-17 19:24:09 +0500

    Challenge 1: Create view in react to upload excel attendance sheet and store data in database

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -5,10 +5,35 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Attendance;
 use App\Models\Employee;
+use DateTime;
 
 class AttendanceController extends Controller
 {
     public function index() {
+        $attendance = Attendance::with('employee')->get();
+        return response()->json($attendance);
+    }
+
+    public function importAttendance(Request $request) {
+        $data = $request->all();
+
+        foreach($data as $record) {
+            // return $record;
+            $employee = Employee::find($record['EMPLOYEE_ID']);
+
+            //Its a temrory code just to avoid datetime error 
+            //Issue with the csv/xls file date column
+            $date = new DateTime();
+            $checkin = $date->setTimestamp($record['CHECKIN']);
+            $checkout = $date->setTimestamp($record['CHECKOUT']);
+            
+            $attendance = $employee->attendances()->create([
+                'checkin' => $checkin,
+                'checkout' => $checkout,
+                'schedule_id' => $record['SCHEDULE_ID']
+            ]);
+        }
+
         $attendance = Attendance::with('employee')->get();
         return response()->json($attendance);
     }
--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -8,7 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Attendance extends Model
 {
     use HasFactory;
-
+    protected $fillable = ['checkin', 'checkout', 'schedule_id'];
+    public $timestamps = false;
+    
     public function employee() {
         return $this->belongsTo(Employee::class);
     }
