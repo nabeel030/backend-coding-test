8e063f7640853307fa93e8c1d5e1154d045f1077
nabeel030 <nabeelahmed030@gmail.com>
2023-05-17 17:23:48 +0500

    Challenge 3: Creating Migration for given structure

---
+++ database/migrations/2023_05_17_121406_add_poeple_id_column_to_employees_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('employees', function (Blueprint $table) {
+            $table->unsignedBigInteger('people_id');
+            $table->unsignedBigInteger('company_id');
+
+            $table->foreign('people_id')->references('id')->on('peoples');
+            $table->foreign('company_id')->references('id')->on('companies');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
